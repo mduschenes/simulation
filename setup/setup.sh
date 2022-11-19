8bf3fcf1926b929cae608648159ccd2ac8f32147
mduschenes <mduschen@uwaterloo.ca>
2022-11-18 19:50:04 -0500

    new setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,11 +11,11 @@ install=${2:-"reinstall"}
 # Silent yes to all commands ["yes","no"]
 yes=${3}
 
-# pkgs=${HOME}/miniconda3
-# envs=${HOME}/miniconda/envs
+pkgs=${HOME}/miniconda3
+envs=${HOME}/miniconda/envs
 
-pkgs=/pkgs/anaconda3
-envs=${HOME}/env
+# pkgs=/pkgs/anaconda3
+# envs=${HOME}/env
 
 channels=(intel conda-forge)
 requirements=requirements.txt
