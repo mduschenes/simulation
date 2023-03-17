3a50e543b7dcf761b93ae7329a9de4b3e3158efb
mduschenes <mduschen@uwaterloo.ca>
2023-03-17 03:44:33 -0400

    fixed optimizer bugs

--- src/optimize.py
+++ src/optimize.py
@@ -104,8 +104,6 @@ class LineSearcher(System):
 		'''
 		returns = dict(zip(self.returns,returns))
 
-		return returns
-
 		attr = 'alpha'
 		if (returns[attr] is None) or (is_naninf(returns[attr])) or (returns[attr] < self.hyperparameters['bounds'][attr][0]) or (returns[attr] > self.hyperparameters['bounds'][attr][1]):		
 			if len(alpha) > 1:
@@ -366,7 +364,7 @@ class GradSearch(GradSearcher):
 		grad_searches = {
 			'grad_search':Fletcher_Reeves,'fletcher_reeves':Fletcher_Reeves,
 			'polak_ribiere':Polak_Ribiere,'polak_ribiere_fletcher_reeves':Polak_Ribiere_Fletcher_Reeves,
-			'hestenes_stiefel':Hestenes_Stiefel,'dai_yuan':Dai_Yuan,'hager_zjang':Hager_Zhang,
+			'hestenes_stiefel':Hestenes_Stiefel,'dai_yuan':Dai_Yuan,'hager_zhang':Hager_Zhang,
 			None:Null_Grad_Search,
 			}
 
