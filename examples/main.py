b3c4c99930b18aa34ae645385a13778827943f9f
mduschenes <mduschen@uwaterloo.ca>
2023-12-14 04:22:58 -0500

    new examples

---
+++ examples/main.py
@@ -0,0 +1,78 @@
+import os,sys
+ROOT = os.path.dirname(os.path.abspath(__file__))
+PATHS = ['','..']
+for PATH in PATHS:
+	sys.path.append(os.path.abspath(os.path.join(ROOT,PATH)))
+
+from src.utils import argparser
+from src.io import load
+from src.system import Dict
+from src.iterables import namespace
+from src.optimize import Optimizer,Objective,Metric,Callback
+
+# Parse command line arguments (default: settings.json)
+arguments = {'--settings':'settings.json'}
+arguments = argparser(arguments)
+settings = arguments['settings']
+
+# Load settings file (wrap in Dict class for object-style key-value pair attributes)
+settings = load(settings,wrapper=Dict)
+
+# Load classes (from path of class i.e) src.quantum.py)
+model = load(settings.cls.model)
+state = load(settings.cls.state)
+label = load(settings.cls.label)
+callback = load(settings.cls.callback)
+
+# Get optimizer and system settings
+hyperparameters = settings.optimize
+system = settings.system
+
+# Initialize model classes
+model = model(**{**settings.model,**dict(system=system)})
+state = state(**{
+	**namespace(state,model),
+	**settings.state,**dict(model=model,system=system)
+	})
+label = label(**{
+	**namespace(label,model),
+	**settings.label,
+	**dict(model=model,system=system)
+	})
+
+# Initialize label and model with state
+label.__initialize__(state=state)
+model.__initialize__(state=state)
+
+# Set optimizer arguments
+func = model.parameters.constraints
+callback = callback(**{
+	**namespace(callback,model),
+	**settings.callback,
+	**dict(model=model,system=system)
+	})
+arguments = ()
+keywords = {}
+
+# Initialize optimizer classes
+metric = Metric(state=state,label=label,
+	arguments=arguments,keywords=keywords,
+	hyperparameters=hyperparameters,system=system)
+func = Objective(model,
+	func=func,callback=callback,metric=metric,
+	hyperparameters=hyperparameters,system=system)
+callback = Callback(model,
+	func=func,callback=callback,metric=metric,
+	arguments=arguments,keywords=keywords,
+	hyperparameters=hyperparameters,system=system)
+optimizer = Optimizer(func=func,callback=callback,
+	arguments=arguments,keywords=keywords,
+	hyperparameters=hyperparameters,system=system)
+
+# Get model parameters and state
+parameters = model.parameters()
+state = model.state()
+
+# Run optimizer
+parameters = optimizer(parameters,state=state)
+
