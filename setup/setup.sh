32bd03f5f995dddd682cba59360a6723b190a1b8
Matthew <mduschen@gra-login1.graham.sharcnet>
2024-07-26 11:03:11 -0400

    new setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,135 +1,46 @@
 #!/bin/bash
 
-# Setup variables
-
 # Environment Name
-env=${1:-jax}
+env=${1:-env}
 
-# Install Type ["install","reinstall","uninstall"]
-install=${2:-"reinstall"}
-
-# Silent yes to all commands ["yes","no"]
-yes=${3}
-
-# pkgs=${HOME}/miniconda3
-# envs=${HOME}/miniconda/envs
-
-# pkgs=/pkgs/anaconda3
 envs=${HOME}/env
-version=3.8
 
-channels=(intel conda-forge)
-requirements=requirements.txt
+requirements=${2:-requirements.txt}
+device=${3:-cpu}
 
-# Load python
-module load python/${version}
+# Load modules
+if [[ ${device} == "cpu" ]]
+then
+	modules=(python)
+elif [[ ${device} == "gpu" ]]
+then
+	modules=(python cuda)
+else
+	modules=(python)
+fi
+
+module purge
+module load ${modules[@]}
+
 
 # Setup paths
 mkdir -p ${envs}
 
-if [[ -z $(grep ${pkgs}/bin <<< ${PATH}) ]] && ( [[ -f ${pkgs}/bin ]] || [[ -d ${pkgs}/bin ]] )
-then
-	export PATH=${PATH}
-fi
-
-if [[ -z $(grep ${pkgs}/lib <<< ${PATH}) ]] && ( [[ -f ${pkgs}/lib ]] || [[ -d ${pkgs}/lib ]] )
-then
-	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
-fi
-
-if [[ -z $(grep ${envs}/${env} <<< ${PYTHONPATH}) ]] && ( [[ -f ${envs}/${env} ]] || [[ -d ${envs}/${env} ]] )
-then
-	export PYTHONPATH=${envs}/${env}:$PYTHONPATH
-fi
-
-
 # Setup environment
-if [ "${install}" == "reinstall" ]
-then
-	deactivate &>/dev/null 2>&1
-	
-	rm -rf ${envs}
-
-	virtualenv --no-download ${envs}/${env}
-
-elif [ "${install}" == "uninstall" ]
-then
-	deactivate &>/dev/null 2>&1
-
-	rm -rf ${envs}/env
-fi
+deactivate &>/dev/null 2>&1
+rm -rf ${envs}/${env}
+virtualenv --no-download ${envs}/${env}
 
 # Activate environment
-# conda activate ${env}
-module load python/${version}
 source ${envs}/${env}/bin/activate
 
-pip -V
-
-# Install packages
-
-# Get line-break separated groups of requirements to install individually
-awk -v requirements="${requirements}" -v RS= '{print > (requirements".tmp." NR "")}' ${requirements}
-requirements=(${requirements}.tmp.*)
-
-# Get installation options
+# Install environment
 options=()
 options+=(--no-index)
+options+=(-r ${requirements})
 
-# Install packages
-for file in ${requirements[@]}
-do
-	pip install -r ${file} ${options[@]}
-	options=()
-
-done
-
-rm ${requirements[@]} 
-
-# conda update --all
-
-# Install rmate (from https://stackoverflow.com/questions/37458814/how-to-open-remote-files-in-sublime-text-3)
-path=${HOME}/.local/bin/rmate
-url=\https://raw.github.com/aurora/rmate/master/rmate
-
-mkdir -p $(dirname ${path})
-wget -O ${path} ${url}
-chmod a+x ${path}
-
-# Install latex packages (from http://physino.xyz/learning/2021/12/22/use-LaTeX-on-a-Linux-HPC-cluster/)
-# if [ "${install}" == "reinstall" ]
-# do
-# 	var=TEXMFHOME
-# 	default=${HOME}/texmf
-# 	pkgs=(physics)
-
-# 	path=$(kpsewhich --var-value ${var})
-# 	path=${path:-${default}}
-
-# 	mkdir -p ${path}
-
-# 	export ${var}=${path}
-
-# 	pwd=${PWD}
-
-# 	cd ${path}
-
-# 	texhash . # or mktexlsr .
-
-# 	tlmgr --init-usertree
-
-# 	tlmgr --usermode install ${pkgs[@]}
-
-# 	cd ${pwd}
-# done
+pip install ${options[@]}
 
 
-# Copy .bashrc
-# source=.bashrc
-# destination=${HOME}
-
-# cp ${source} ${destination}
-
-
-# Git
-git config --global merge.ours.driver true
+# Test environment
+pytest -rA -W ignore::DeprecationWarning test.py
