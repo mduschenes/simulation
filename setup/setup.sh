c941d15629fe7cac1086537ac78da2ebe367ca1b
Matthew <mduschen@gra-login1.graham.sharcnet>
2024-07-27 19:26:37 -0400

    updated setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,43 +1,246 @@
 #!/bin/bash
 
 # Environment
-env=${1:-env}
-requirements=${2:-requirements.txt}
-envs=${3:-${HOME}/env/envs}
-device=${4:-cpu}
-exe=${5:-conda}
 
-# Load modules
-if [[ ${device} == "cpu" ]]
+env=
+requirements=requirements.txt
+architecture=cpu
+installer=pip
+envs=${HOME}/envs
+pkgs=${HOME}/conda
+dry_run=
+
+function usage(){
+
+	variable=(e r a i v p)
+	variables=(env requirements architecture installer envs pkgs)
+
+	file=$(basename ${0})
+	directory=$(dirname ${0})
+
+	echo Usage: ${file}
+	for i in ${!variables[@]}
+	do
+		case ${variables[$i]} in
+			env) string="environment name: <env>" ;;
+			requirements) string="requirements file: requirements.txt" ;;
+			architecture) string="architecture type: cpu|gpu" ;;
+			installer) string="installer type: pip|conda" ;;
+			envs) string="path to environments: \${HOME}/envs" ;;
+			pkgs) string="path to root packages: \${HOME}/conda" ;;
+			dry-run) string="dry-run boolean" ;;
+			help) string="man page" ;;
+			*) string=${!variables[$i]};;
+		esac
+		printf "\t%s | %-16s %s\n" "-${variables[$i]:0:1}" "--${variables[$i]}"  "${string[@]}"
+	done
+}
+
+if [[ $# -eq 0 ]]
 then
-	modules=(python)
-elif [[ ${device} == "gpu" ]]
-then
-	modules=(python/3.11.5 cuda/12.2)
-else
-	modules=(python)
+	usage
+	exit
 fi
 
-module purge
-module load ${modules[@]}
+eval set -- "$(getopt --options eraivpdh --longoptions env:,requirements:,architecture:,installer:,envs:,pkgs:,dry-run,help, -- "$@")"
+while [[ $# -gt 0 ]]; do case ${1} in 
+-e|--env) env=${2}; shift 2;; 
+-r|--requirements) requirements=${2}; shift 2;; 
+-a|--architecture) architecture=${2}; shift 2;;
+-i|--installer) installer=${2}; shift 2;;
+-v|--envs) envs=${2}; shift 2;;
+-p|--pkgs) pkgs=${2}; shift 2;;
+-d|--dry-run) dry_run=true; shift;;
+-h|--help) usage; exit; shift;; 
+--) shift;break;; *);;
+esac done
+
+env=${env:-${1}}
+
+if [[ -z ${env} ]]
+then
+	usage
+	exit
+fi
+
+echo env ${env}
+echo requirements ${requirements}
+echo architecture ${architecture}
+echo installer ${installer}
+echo envs ${envs}
+echo pkgs ${pkgs}
+echo dry-run ${dry_run}
+exit
 
 
-# Setup environment
-mkdir -p ${envs}
-deactivate &>/dev/null 2>&1
-rm -rf ${envs}/${env}
-virtualenv --no-download ${envs}/${env}
+# # Setup environment
+# dir=$(dirname ${env})
+# env=$(basename ${env})
+# if [[ ! ${dir} == . ]];then envs=${dir};fi
 
-# Activate environment
-source ${envs}/${env}/bin/activate
+# case ${installer} in
+# 	pip)
+# 		case ${architecture} in
+# 			gpu)
+# 				modules=(python cuda cudnn)
+# 				module purge &>/dev/null
+# 				module load ${modules[@]}
+# 				;;
+# 			cpu)
+# 				modules=(python)
+# 				module purge &>/dev/null
+# 				module load ${modules[@]}
+# 				;;
+# 			*)
+# 				modules=(python)
+# 				module purge &>/dev/null
+# 				module load ${modules[@]}
+# 			;;
+# 		esac
 
-# Install environment
-options=()
-options+=(--no-index)
-options+=(-r ${requirements})
+# 		mkdir -p ${envs}
+# 		deactivate &>/dev/null 2>&1
+# 		rm -rf ${envs}/${env}
+# 		pip install --upgrade pip --no-index
+# 		virtualenv --no-download ${envs}/${env}
 
-pip install ${options[@]}
+# 		source ${envs}/${env}/bin/activate
 
-# Test environment
-pytest -rA -W ignore::DeprecationWarning test.py
-rm -rf __pycache__ .pytest_cache
+# 		options=()
+# 		options+=(--no-index)
+# 		options+=(-r ${requirements})
+
+# 		pip install ${options[@]}
+# 		;;
+# 	conda)
+# 		mkdir -p ${envs}
+# 		conda deactivate
+# 		conda config --remove envs_dirs ${envs} &>/dev/null 2>&1
+# 		conda config --append envs_dirs ${envs} &>/dev/null 2>&1
+# 		conda remove --name ${env} --all
+# 		conda create --prefix ${envs}/${env}
+
+# 		conda activate ${env}
+
+# 		options=()
+# 		options+=(--channel conda-forge)
+# 		options+=(--file ${requirements})
+
+# 		conda install ${options[@]}
+# 		;;
+# 	*)
+# 		;;
+# esac
+
+
+# # Test environment
+# test=test.py
+# if [[ -f ${test} ]]
+# then
+# 	options=(-rA -W ignore::DeprecationWarning)
+# 	pytest ${options[@]} ${test}
+# 	rm -rf __pycache__ .pytest_cache
+# fi
+
+
+# # Setup environment
+# env=${1:-env}
+# default=base
+# envs=${HOME}/envs
+# pkgs=${6:-${HOME}/conda}
+# pkgs=${HOME}/envs/${env}
+
+# if [[ ! -d ${envs}/${env} ]];then env=${default};fi
+
+# case ${installer} in
+# 	pip)
+# 		case ${architecture} in
+# 			gpu)
+# 				modules=(python cuda cudnn)
+# 				module purge &>/dev/null
+# 				module load ${modules[@]}
+# 				;;
+# 			cpu)
+# 				modules=(python)
+# 				module purge &>/dev/null
+# 				module load ${modules[@]}
+# 				;;
+# 			*)
+# 				modules=(python)
+# 				module purge &>/dev/null
+# 				module load ${modules[@]}
+# 			;;
+# 		esac
+# 		source ${envs}/${env}/bin/activate
+# 		;;
+# 	conda)
+# 		conda activate ${env}
+# 		;;
+# 	*)
+# 		;;
+# esac
+
+# # Setup variables
+# case ${architecture} in
+# 	gpu)
+# 		variables=(PATH LD_LIBRARY_PATH PYTHONPATH)
+# 		strings=("${envs}/${env}/bin" "${envs}/${env}/lib" "${envs}/${env}")
+# 	;;
+# 	cpu)
+# 		variables=()
+# 		strings=()
+# 	;;
+# 	*)
+# 		variables=()
+# 		strings=()
+# 	;;
+# esac
+
+# for i in ${!variables[@]}
+# do
+# 	variable=${variables[${i}]}
+# 	string=${strings[${i}]}
+# 	if [[ -z ${!variable} ]]
+# 	then
+# 		export ${variable}=${string}
+# 	elif [[ -z $(grep : <<< ${!variable}) ]] && [[ -z $(grep ${string} <<< ${!variable}) ]]
+# 	then
+# 		export ${variable}=${string}:${!variable}		
+# 	elif [[ ! -z $(grep : <<< ${!variable}) ]] && [[ -z $(grep ${string}: <<< ${!variable}) ]]
+# 	then
+# 		export ${variable}=${string}:${!variable}
+# 	fi
+# done
+
+
+# # Setup flags
+# case ${architecture} in
+# 	gpu)
+# 		variables=(XLA_FLAGS)
+# 		strings=("--xla_gpu_cuda_data_dir=${CUDA_HOME}")
+# 	;;
+# 	cpu)
+# 		variables=()
+# 		strings=()
+# 	;;
+# 	*)
+# 		variables=()
+# 		strings=()
+# 	;;
+# esac
+
+# for i in ${!variables[@]}
+# do
+# 	variable=${variables[${i}]}
+# 	string=${strings[${i}]}
+# 	if [[ -z ${!variable} ]]
+# 	then
+# 		export ${variable}=${string}
+# 	elif [[ -z $(grep " " <<< ${!variable}) ]] && [[ -z $(grep "${string}" <<< ${!variable}) ]]
+# 	then
+# 		export ${variable}="${string} ${!variable}"
+# 	elif [[ ! -z $(grep " " <<< ${!variable}) ]] && [[ -z $(grep "${string} " <<< ${!variable}) ]]
+# 	then
+# 		export ${variable}="${string} ${!variable}"
+# 	fi
+# done
--- setup/test.py
+++ setup/test.py
@@ -11,17 +11,52 @@ def test_numpy(*args,**kwargs):
 	return
 
 def test_jax(*args,**kwargs):
-	import os
-	envs = {
-		'JAX_PLATFORMS':'cpu',
-		'JAX_PLATFORM_NAME':'cpu',
-		'TF_CPP_MIN_LOG_LEVEL':5
-	}
-	for var in envs:
-		os.environ[var] = str(envs[var])
-	import jax
-	import jax.numpy as jnp
-	print(jax.devices())
+
+	def func(device):
+		import os
+		environs = {
+				'JAX_PLATFORMS':device,
+				'JAX_PLATFORM_NAME':device,
+				'TF_CPP_MIN_LOG_LEVEL':5
+			}
+			
+		for name in environs:
+			os.environ[name] = str(environs[name])
+		import jax
+		from jax.lib import xla_bridge
+		import jax.numpy as np
+		
+		configs = {
+			'jax_disable_jit':False,
+			'jax_platforms':device,
+			'jax_platform_name':device,
+			'jax_enable_x64': True,
+			}
+		for name in configs:
+			jax.config.update(name,configs[name])
+
+		print(xla_bridge.get_backend().platform)
+		print(jax.devices())
+
+		array = np.array([1,2,3])
+		print(array.devices())
+		array *= array
+
+		del jax
+		return
+	
+	devices = ['cuda','gpu','cpu']
+	# devices = ['cpu','gpu',]
+	for device in devices:
+		try:
+			func(device)
+			return
+		except Exception as exception:
+			print(exception)
+			continue
+
+	raise AttributeError("JAX IMPORT ERROR")
+
 	return
 
 def test_matplotlib(*args,**kwargs):
@@ -65,6 +100,8 @@ def test_pytables(*args,**kwargs):
 def main(*args,**kwargs):
 	test_python()
 	test_numpy()
+	test_numpy()
+	test_matplotlib()
 	test_pytables()
 	return
 
