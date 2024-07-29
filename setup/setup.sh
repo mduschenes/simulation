9fa9c7a5c84c0ba680f29f448ef6d2e333a5ab98
Matthew <mduschen@gra-login1.graham.sharcnet>
2024-07-29 15:15:24 -0400

    updated requirements.txt

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+
+env=${1:-env}
+requirements=${2:-requirements.txt}
+envs=${3:-${HOME}/envs}
+# modules=(${4:-cuda-12.3})
+modules=(${4:-})
+
 module purge
 module load python cuda cudnn
-mkdir -p /home/mduschen/envs
+mkdir -p ${envs}
 deactivate
-rm -rf /home/mduschen/envs/env
-virtualenv --no-download /home/mduschen/envs/env
-source /home/mduschen/envs/env/bin/activate
+rm -rf ${envs}/${env}
+virtualenv --no-download ${envs}/${env}
+source ${envs}/${env}/bin/activate
 pip install --no-index --requirement requirements.txt
 pytest -rA -W ignore::DeprecationWarning test.py
