fixed non-numerical object data processing

src/process.py
@@ -1292,7 +1292,7 @@ def apply(keys,data,plots,processes,verbose=None):
 		try:
 			obj = np.array(list(obj))
 			obj = to_tuple(obj.mean(axis=0))
-		except ValueError:
+		except:
 			obj = np.array([list(obj)[0]])
 			obj = obj[0]
 		return obj
@@ -1300,7 +1300,7 @@ def apply(keys,data,plots,processes,verbose=None):
 		try:
 			obj = np.array(list(obj))
 			obj = to_tuple(obj.std(axis=0,ddof=obj.shape[0]>1))
-		except ValueError:
+		except:
 			obj = np.array([list(obj)[0]])
 			obj = to_tuple(np.zeros(obj.shape[1:],dtype=obj.dtype))
 		return obj
@@ -1308,7 +1308,7 @@ def apply(keys,data,plots,processes,verbose=None):
 		try:
 			obj = np.array(list(obj))
 			obj = to_tuple(obj.std(axis=0,ddof=obj.shape[0]>1))
-		except ValueError:
+		except:
 			obj = np.array([list(obj)[0]])
 			obj = to_tuple(np.zeros(obj.shape[1:],dtype=obj.dtype))		
 		return obj	