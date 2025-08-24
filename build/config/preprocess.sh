77aad4928e4cce00fe9181c11cd8cf35a8facb56
Matthew Duschenes <mduschen@uwaterloo.ca>
2025-08-23 23:08:52 -0400

    new config

--- build/config/preprocess.sh
+++ build/config/preprocess.sh
@@ -9,4 +9,4 @@ export SLURM_ARRAY_TASK_STEP=
 export SLURM_ARRAY_TASK_COUNT=
 export SLURM_ARRAY_TASK_SLICE=
 export SLURM_ARRAY_TASK_SIZE=1
-sbatch -J job --export=JOB_CWD=.,JOB_ENV=tensor,JOB_MODULES="python hdf5 cuda cudnn",JOB_SRC=~/code/tensor/src,JOB_CMD=process.py,JOB_PROCESSES=1,JOB_ARGS="--data=**/data.hdf5 --plots=plot.json --processes=process.json --pwd=. --cwd=. --verbose",NUMPY_BACKEND=jax < postprocess.slurm
+sbatch -J job --export=JOB_CWD=.,JOB_ENV=tensor,JOB_MODULES="python hdf5 cuda cudnn",JOB_SRC=~/code/tensor/src,JOB_CMD=process.py,JOB_PROCESSES=1,JOB_ARGS="--data=**/data.hdf5 --plots=plot.json --processes=process.json --pwd=. --cwd=. --verbose",NUMPY_BACKEND=jax < preprocess.slurm
