fixed normalization of finite data continuous probabilities and processig of on axes attributes

src/functions.py
@@ -27,7 +27,7 @@ from src.utils import addition,multiply,divide,power,matmul,sqrt,floor,exp,log,l
 from src.utils import to_tuple,is_nan,is_naninf,asscalar
 from src.utils import grouper,conditions,flatten,concatenate,inplace,epsilon
 from src.utils import orng as rng
-from src.utils import arrays,scalars,dataframes,integers,floats,nonzero,delim,nan
+from src.utils import arrays,scalars,dataframes,iterables,numbers,integers,floats,nonzero,delim,nan
 
 from src.iterables import permuter,setter,getter,search,Dictionary
 
@@ -282,7 +282,7 @@ def func_information_x(data,*args,**kwargs):
 	return data
 
 def func_information_y(data,*args,**kwargs):
-	data = tuple(data)
+	data = tuple(data) if isinstance(data,iterables) and len(data)>1 else data
 	return data
 
 def func_information_xerr(data,*args,**kwargs):
@@ -290,13 +290,13 @@ def func_information_xerr(data,*args,**kwargs):
 	return data
 
 def func_information_yerr(data,*args,**kwargs):
-	data = tuple(data)
+	data = tuple(data) if isinstance(data,iterables) and len(data)>1 else data
 	return data
 
 def func_information_process_x(data,values,metadata,properties,*args,**kwargs):
 	keys = metadata['x']
 	values = {} if not isinstance(values,dict) else values
-	data = [data for key in keys] if data is None else data
+	data = [data for key in keys] if not isinstance(data,iterables) else data
 	for key,i in zip(keys,data):
 		values[key] = i
 	data = values
@@ -305,7 +305,7 @@ def func_information_process_x(data,values,metadata,properties,*args,**kwargs):
 def func_information_process_y(data,values,metadata,properties,*args,**kwargs):
 	keys = metadata['x']
 	values = {} if not isinstance(values,dict) else values
-	data = [data for key in keys] if data is None else data
+	data = [data for key in keys] if not isinstance(data,iterables) else data
 	for key,i in zip(keys,data):
 		values[key] = np.array([*values.get(key,[]),*flatten(i)])
 	data = values
@@ -314,7 +314,7 @@ def func_information_process_y(data,values,metadata,properties,*args,**kwargs):
 def func_information_process_xerr(data,values,metadata,properties,*args,**kwargs):
 	keys = metadata['x']
 	values = {} if not isinstance(values,dict) else values
-	data = [data for key in keys] if data is None else data
+	data = [data for key in keys] if not isinstance(data,iterables) else data
 	for key,i in zip(keys,data):
 		values[key] = i
 	data = values
@@ -323,19 +323,19 @@ def func_information_process_xerr(data,values,metadata,properties,*args,**kwargs
 def func_information_process_yerr(data,values,metadata,properties,*args,**kwargs):
 	keys = metadata['x']
 	values = {} if not isinstance(values,dict) else values
-	data = [data for key in keys] if data is None else data
+	data = [data for key in keys] if not isinstance(data,iterables) else data
 	for key,i in zip(keys,data):
 		values[key] = np.array([*values.get(key,[]),*flatten(i)])
 	data = values
 	return data
 
-def func_information_function(data,*args,function=None,**kwargs):
+def func_information_function(data,*args,attribute=None,function=None,**kwargs):
 
 	keys = data['y']
 	keys = list(keys) if isinstance(keys,dict) else range(len(keys)) if keys is not None else None
 	keys = natsorted(keys) if keys is not None else None
 
-	def parse(attr,key,data):
+	def parse(attr,data):
 		if data is None:
 			data = None
 		elif all(i is None for i in data):
@@ -345,11 +345,14 @@ def func_information_function(data,*args,function=None,**kwargs):
 			data[(is_naninf(data))|(data<epsilon(data.dtype))] = {'x':0,'y':nan,'xerr':0,'yerr':0}.get(attr,0)
 		return data
 
-	default = lambda data,*args,**kwargs: 1
+	if attribute is None or getter(data,attribute,delimiter=delim) is None:
+		attribute = None
+
+	func = lambda data,*args,**kwargs: 1
 	if function is None:
-		function = default
+		function = func
 	elif isinstance(function,str):
-		function = load(function,default=default)
+		function = load(function,default=func)
 	def decorator(function):
 		def wrapper(attr,key,data,*args,**kwargs):
 			data = {attr:data[attr] if not isinstance(data[attr],dict) or key not in data[attr] else data[attr][key] for attr in data}
@@ -369,7 +372,7 @@ def func_information_function(data,*args,function=None,**kwargs):
 	attr = 'y'
 	def func(attr,key,data):
 		number,size = function(attr,key,data),data[attr][key].size
-		data = np.mean(data[attr][key])
+		data = np.mean(data[attr][key]) +  np.log(np.sum(getter(data,attribute,delimiter=delim)[key]))
 		data = data/np.log(number) - 1
 		return data
 	funcs[attr] = func
@@ -383,14 +386,23 @@ def func_information_function(data,*args,function=None,**kwargs):
 	attr = 'yerr'
 	def func(attr,key,data):
 		number,size = function(attr,key,data),data[attr][key].size
+		obj = np.mean(data[attr][key])
 		data = np.mean(data[attr][key]) - np.mean(data[attr[0]][key])**2
 		data = np.sqrt(data/(size*number))/np.log(size)
 		return data
-	funcs[attr] = func
+	if attr:
+		funcs[attr] = func
 
-	funcs = {attr:parse(attr,key,[funcs[attr](attr,key,data) for key in keys]) for attr in funcs if attr in data} if keys is not None else {}
+	if attribute:
+		attr = attribute
+		def func(attr,key,data):
+			data = np.log(np.sum(getter(data,attr,delimiter=delim)[key]))
+			return data
+		funcs[attr] = func
 
-	data.update(funcs)
+	funcs = {attr:parse(attr,[funcs[attr](attr,key,data) for key in keys]) for attr in funcs if getter(data,attr,delimiter=delim) is not None} if keys is not None else {}
+
+	setter(data,funcs,delimiter=delim,default=True)
 
 	return data
 
@@ -405,12 +417,12 @@ def func_information(obj,*args,**kwargs):
 	eps = epsilon(obj.dtype)
 	def func(x,n=n,eps=eps):
 		x = (n-1)*((1-x)**(n-2)) # (n/(1-np.exp(-n)))*np.exp(-n*obj) # n*np.exp(-n*obj)
-		x /= addition(x)
+		# x /= addition(x)
 		x = inplace(x,x<eps,1)
 		return x
-	key = ['','err']
+	key = [None,'error','norm']
 	value = information(func,obj)
-	value = addition(value)/n,addition(value**2)/n
+	value = addition(value)/n,addition(value**2)/n,addition(func(obj))
 	data = dict(zip(key,value))
 	return data
 
@@ -999,19 +1011,3 @@ def layout(iterable,sort=False,group=False):
 
 def test(*args,**kwargs):
 	return args,kwargs
-
-# def func(data,*args,**kwargs):
-# 	if data is None:
-# 		data = {}
-	
-# 	default = {'index':[],'value':[]}
-
-# 	data.update({attr: default[attr] for attr in default if attr not in data})
-
-# 	data.update({attr: [*data[attr]] for attr in data})
-
-# 	for i in kwargs.get('indexes'):
-# 		data['index'].append(max(data['index'],default=-1)+1)
-# 		data['value'].append(i)
-
-# 	return

src/process.py
@@ -1652,9 +1652,12 @@ def apply(data,plots,processes,verbose=None):
 						continue
 
 					for axes in statistics:
+
 						if axes in dimensions or not obj[string].get(axes):
 							continue
+
 						axis = [ax for ax in dimensions if axes.startswith(ax) and obj[string].get(ax)]
+
 						for ax in axis:
 
 							obj[string][ax] = {
@@ -1666,6 +1669,7 @@ def apply(data,plots,processes,verbose=None):
 									**{attribute:obj[string][ax][function][attribute] for attribute in obj[string][ax][function] if attribute not in nulls},
 									} for func in [''] for function in nulls if function in obj[string][ax]},
 								}
+
 						if axis:
 							obj[string][axes] = {
 								**{func:{
@@ -1676,6 +1680,16 @@ def apply(data,plots,processes,verbose=None):
 										**{attribute:obj[string][axes][function][attribute] for attribute in obj[string][axes][function] if attribute not in nulls},
 										} for func in ['err'] for function in nulls if function in obj[string][axes]},
 								}
+
+					for axes in obj[string]:
+
+						if axes in statistics:
+							continue
+
+						if not isinstance(obj[string][axes],dict) or axes not in obj[string][axes]:
+							obj[string][axes] = {axes:obj[string][axes]}
+
+						obj[string][axes] = wrap(obj[string][axes],args=args,kwargs=kwargs,defaults=defaults)
 
 			function = wrap(function,args=args,kwargs=kwargs,defaults=defaults)
 
@@ -1781,9 +1795,13 @@ def apply(data,plots,processes,verbose=None):
 			shapes = {prop: tuple((i[0] if len(set(i))==1 else i for i in shapes[prop])) for prop in shapes}
 
 			applications = {
-				**{attr : [(attr, {'array':mean,'object':first,'dtype':mean}[dtypes[attr]] 
+				**{attr : ([(attr, {'array':mean,'object':first,'dtype':mean}[dtypes[attr]]
 						  if attr not in by else {'array':first,'object':first,'dtype':first}[dtypes[attr]])] 
-						  for attr in attributes},
+						  if attr not in funcs[string] else
+						  [(attribute,funcs[string][attr][func][attribute]) for func in funcs[string][attr] for attribute in funcs[string][attr][func]])
+						  for attr in attributes
+						  for string in funcs
+						  },
 				**{attr : [(delim.join(((attr,string,func))),
 							{
 							'array':{
@@ -1799,7 +1817,7 @@ def apply(data,plots,processes,verbose=None):
 								**{'err':funcs[string][ax][func][attribute] for attribute in funcs[string][ax][func]},
 								}[func],
 							}[dtypes[attr]])
-							for string in funcs for ax in [ax for ax in statistics if ax.startswith(axes) and funcs[string].get(ax)] for func in funcs[string][ax]
+							for string in funcs for ax in funcs[string] if ax in statistics for func in funcs[string][ax] if keys[name][ax]==attr
 							if delim.join(((attr,string,func))) not in data]
 							for axes in dimensions
 							for attr in [keys[name][ax] for ax in statistics if ax.startswith(axes)]
@@ -1909,9 +1927,14 @@ def apply(data,plots,processes,verbose=None):
 			plts = copy(getter(plots,key,delimiter=delim))
 
 			def parser(string,axes,func):
-				attr = keys[name][axes]
-				source = delim.join(((attr,string,func))) if attr in [*independent,*dependent,*exceptions] else attr
-				destination = '%s%s'%(axes,func) if attr in [*independent,*dependent,*exceptions] and axes in dimensions else axes
+				if axes in statistics:
+					attr = keys[name][axes]
+					source = delim.join(((attr,string,func))) if attr in [*independent,*dependent,*exceptions] else attr
+					destination = '%s%s'%(axes,func) if attr in [*independent,*dependent,*exceptions] and axes in dimensions else axes
+				else:
+					attr = axes
+					source = axes
+					destination = axes
 				return attr,source,destination
 
 			for i,group in enumerate(groupings[name]):
@@ -1962,6 +1985,7 @@ def apply(data,plots,processes,verbose=None):
 							'label':keys[name][axes] if not isinstance(keys[name][axes],Null) else None
 							} 
 							for axes in funcs[string]
+							if axes in statistics
 							for func in funcs[string][axes]
 							},
 						**{other: {attr: {subattr: keys[name][other][attr][subattr] 
@@ -1974,12 +1998,13 @@ def apply(data,plots,processes,verbose=None):
 					options = dict(deep=False)
 					metadata = grouping.copy(**options)
 
-					options = dict(mapper={source:destination for axes in funcs[string] for func in funcs[string][axes] for attr,source,destination in [parser(string,axes,func)]},axis='columns',inplace=True)
+					options = dict(mapper={source:destination for axes in funcs[string] if axes in statistics for func in funcs[string][axes] for attr,source,destination in [parser(string,axes,func)]},axis='columns',inplace=True)
 					metadata.rename(**options)
 
 					value[destination] = obj
 
 					for axes in funcs[string]:
+
 						for func in funcs[string][axes]:
 
 							attr,source,destination = parser(string,axes,func)
@@ -2005,7 +2030,6 @@ def apply(data,plots,processes,verbose=None):
 									except Exception as exception:
 										obj = None
 								elif isinstance(source,Null):
-									source = delim.join(((dependent[-1],string,func)))
 									obj = None #np.arange(indexing,len(obj.iloc[0])+indexing) if obj.iloc[0] is not None else None
 								else:
 									obj = None #grouping.reset_index().index.to_numpy()
@@ -2023,7 +2047,10 @@ def apply(data,plots,processes,verbose=None):
 								for attribute in process[string][axes][func]:
 									obj = process[string][axes][func][attribute](obj,value.get(destination),metadata,property)
 
-							value[destination] = obj
+							if destination in ALL:
+								value[destination] = obj
+							else:
+								value[other][destination] = obj
 
 					functions[name][key] = function
 
@@ -2037,7 +2064,6 @@ def apply(data,plots,processes,verbose=None):
 
 
 		del data
-
 
 	for name in functions:
 