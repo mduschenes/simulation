9d77b19ba20c6ee717f23ddd088a05ce3663b28d
Matthew Duschenes <mduschen@uwaterloo.ca>
2025-10-19 19:20:34 -0400

    new plot.sh

---
+++ build/plot.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+source ~/.bashrc
+
+path=${1}
+type=${2:-log}
+indices=(${3:-0 4 3 2 1})
+
+options=()
+process=()
+
+for index in ${indices[@]}
+do
+	case ${index} in
+		0)
+			strings=(${index} ${index})
+			;;
+		*)
+			strings=(1e-${index} ${index})
+			;;
+	esac
+
+	case ${type} in
+		log)
+			options=()
+			process=()
+
+			options+=(
+				-i \
+				-e "s/\(\"noise.parameters\"\:\ \)\[.*\]/\1[${strings[0]}]/" \
+				-e "s/\(\"fig.savefig.fname\"\:\).*/\1\"M.noise.${strings[1]}\",/" \
+				${path}/process.json
+			)
+
+			process+=(pdfmerge)
+			name=${path}/plot/plot.sample.array.M.noise.parameters.M.noise
+			string="$(echo ${indices[@]} | sed 's/ /./g')"; process+=("${name}.${string}.pdf");
+			for i in ${indices[@]}
+			do
+				process+=("${name}.${i}.pdf");
+			done
+			;;
+		scale)
+			options=()
+			process=()
+
+			options+=(
+				-i \
+				-e "s/\(\"noise.parameters\"\:\ \)\[.*\]/\1[${strings[0]}]/" \
+				-e "s/\(\"fig.savefig.fname\"\:\).*/\1\"scale.M.noise.${strings[1]}\",/" \
+				${path}/process.json
+			)
+
+			process+=(pdfmerge)
+			name=${path}/plot/plot.sample.array.M.noise.parameters.scale.M.noise
+			string="$(echo ${indices[@]} | sed 's/ /./g')"; process+=("${name}.${string}.pdf");
+			for i in ${indices[@]}
+			do
+				process+=("${name}.${i}.pdf");
+			done
+			;;
+		*)
+			;;
+	esac
+
+	continue
+
+	if [[ ! -s ${options} ]]
+	then
+		sed "${options[@]}"
+
+		exe=./process.py
+		args=(${path})
+
+		${exe} ${args[@]}
+	fi
+
+done
+
+
+if [[ ! -s ${process} ]]
+then
+	${process[@]}
+fi
