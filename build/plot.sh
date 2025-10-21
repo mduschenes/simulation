2637585eb33edbd7f0abb9c529e7a487f8d292ec
Matthew Duschenes <mduschen@uwaterloo.ca>
2025-10-21 11:40:02 -0400

    new plot.sh

--- build/plot.sh
+++ build/plot.sh
@@ -82,3 +82,36 @@ if [[ ! -s ${process} ]]
 then
 	${process[@]}
 fi
+
+# from src.utils import array,flatten,is_naninf
+# from src.io import load,dump,exists
+
+# if not exists(path):
+# 	merge(data,path,*args,**kwargs)
+
+# options = dict(wrapper='df',verbose=True)
+# data = load(path,**options)
+
+# print(data.shape,[*data.columns])
+
+# keys = {key:['%s'%(key),'%s.error'%(key)] for key in ['sample.array.information','sample.state.information']}
+# by = ['N','M','noise.parameters']
+# options = dict(as_index=False,dropna=False)
+# def func(data):
+# 	data = array([*flatten(data)])
+# 	return data
+
+# data = data.groupby(by=by,**options)
+
+# for groups in data.groups:
+
+# 	print(dict(zip(by,groups)))
+
+# 	group = data.get_group(groups)
+
+# 	for key in keys:
+# 		for i in keys[key]:
+# 			value = func(group[i])
+# 			print(i,i[is_naninf(i)].shape)
+# 			print(i)
+# 			print()
