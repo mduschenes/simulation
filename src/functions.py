1d1db3ec344422b5310c62d7cbf938e3168bdf8e
Matthew Duschenes <mduschen@uwaterloo.ca>
2025-12-15 02:19:46 -0500

    new plot settings

--- src/functions.py
+++ src/functions.py
@@ -610,8 +610,7 @@ def func_stats_function(data,*args,function=None,x=None,y=None,xerr=None,yerr=No
 		elif settings.get('func') in ['distance']:
 			data = (1/2)*sum(abs(data/sum(data)-Y/sum(Y)))
 		elif settings.get('func') in ['cumulative']:
-			# data = max(abs(cumsum(data/sum(data))/size-cumsum(Y/sum(Y))/size))
-			data = max(abs(cumsum(data)/size-info.function(X)))
+			data = max(abs((cumsum(data)/sum(data))-info.function(X)))
 		else:
 			data = (1/2)*sum(abs((data/sum(data))-(Y/sum(Y))))
 
