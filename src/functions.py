c95c26e6098c3f5be18d9260906d722b61f5ec85
Matthew Duschenes <mduschen@uwaterloo.ca>
2026-02-01 04:59:49 -0500

    checking bugs in processing

--- src/functions.py
+++ src/functions.py
@@ -613,7 +613,7 @@ def func_stats_function(data,*args,function=None,x=None,y=None,xerr=None,yerr=No
 		elif settings['func'] in ['cumulative']:
 			Y = info.functional(X)
 			data,Y = data/addition(data),Y
-			data = maximum(absolute((cumsum(data)-Y))+absolute(difference(Y,append=Y[-1]-Y[-2])))
+			data = maximum(absolute((cumsum(data)-Y))+absolute(difference(Y,append=0)))
 			print(maximum(absolute(difference(Y,append=0))),data)
 		else:
 			data = None
